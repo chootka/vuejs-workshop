bed75e1f4ccd1763b3d312663e74aa9c730d35dc
chootka <com@chootka.com>
2021-02-15 16:57:40 +0100

    updated readme

--- README.md
+++ README.md
@@ -23,3 +23,7 @@ npm run lint
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
 # vuejs-workshop
+
+### Learn more about Vue.js Develpment
+See Vue.js documentation. [Vue 2](https://vuejs.org/v2/guide/), [Vue 3](https://v3.vuejs.org/guide/introduction.html)
+
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="works">Works</router-link>
+    </nav>
+
+    <router-view></router-view>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
+@font-face {
+  font-family: "NokiaPureH";
+  src: url("~@/assets/fonts/NokiaPureHWeb_Rg.otf") format("truetype");
+}
+@font-face {
+  font-family: "NokiaPureHLt";
+  src: url("~@/assets/fonts/NokiaPureHWeb_Lt.otf") format("truetype");
+}
+@font-face {
+  font-family: "NokiaPureHBold";
+  src: url("~@/assets/fonts/NokiaPureHWeb_Bd.otf") format("truetype");
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: NokiaPureH, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
+
+nav {
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+}
+
+nav a {
+  padding: 10px;
+}
+
 </style>
